dc933f7fcf1ce2fae3fa1ecb21797838230f5596
Ben Nuttall <ben@bennuttall.com>
2016-06-03 17:22:05 +0100

    Add readme and code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wiibot
+
 A Wii remote controlled robot using CamJam kit 3
+
+## Hardware
+
+- Raspberry Pi 3
+- CamJam kit 3 parts:
+    - Motor controller board
+    - Ultrasonic distance sensor
+    - 2 x motors with wheels
+    - Robot chassis
+- Wiimote
+- Buzzer
+
+## Software
+
+- [GPIO Zero](http://gpiozero.readthedocs.io)
+- [cwiid](https://github.com/abstrakraft/cwiid)
+
+## Python 2
+
+It's Python 2 due to [cwiid](https://github.com/abstrakraft/cwiid/issues/26)
---
+++ robot.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python2
+
+from __future__ import print_function
+from gpiozero import CamJamKitRobot, Buzzer, DistanceSensor
+import cwiid
+from subprocess import check_call
+
+def shutdown():
+    buzzer.beep(0.1, 0.1, n=20, background=False)
+    buttons = wii.state['buttons']
+    if buttons & cwiid.BTN_B:
+        check_call(['sudo', 'poweroff'])
+
+def vibrate():
+    print("vibrate")
+    wii.rumble = 1
+
+def stop_vibrate():
+    print("stop vibrate")
+    wii.rumble = 0
+
+robot = CamJamKitRobot()
+buzzer = Buzzer(23)
+ultrasonic = DistanceSensor(echo=17, trigger=18, threshold_distance=0.1)
+
+buzzer.beep(0.1, 0.5)
+wii = None
+while not wii:
+    try:
+        wii = cwiid.Wiimote()
+    except:
+        print("Hold down Wiimote buttons")
+
+buzzer.off()
+wii.rpt_mode = cwiid.RPT_BTN
+
+ultrasonic.when_in_range = vibrate
+ultrasonic.when_out_of_range = stop_vibrate
+
+while True:
+    print(ultrasonic.distance)
+    buttons = wii.state['buttons']
+    if buttons & cwiid.BTN_UP:
+        robot.forward()
+    elif buttons & cwiid.BTN_DOWN:
+        robot.backward()
+    elif buttons & cwiid.BTN_LEFT:
+        robot.left()
+    elif buttons & cwiid.BTN_RIGHT:
+        robot.right()
+    elif buttons & cwiid.BTN_B:
+        shutdown()
+    else:
+        robot.stop()
